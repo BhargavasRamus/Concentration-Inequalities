193fe7ff6af54288bc2b5d5fc2486540d925a471
Pamula Bhargav Ram <29677592+BhargavasRamus@users.noreply.github.com>
2019-01-21 00:04:48 +0530

    Update Q_6b.py

--- Assignment_1/Q_6b.py
+++ Assignment_1/Q_6b.py
@@ -1,3 +1,19 @@
-import numpy as np
-import matplotlib.pyplot as mp
-import scipy as sp
+import numpy as np 
+import matplotlib.pyplot as plt 
+import IPython.core.pylabtools import figsize
+import pandas as pd 
+import scipy as sp 
+
+n = 1000
+results = []
+for n in range(1,10000):
+	throws = np.random.randint(1,7,n)
+	mean_throws = throws.mean()
+	results.append(mean_throws)
+
+df = pd.DataFrame({'throws':results})
+
+figsize(11,9)
+df.plot(title='LLN (Guassian)',color='r')
+plt.xlabel("N")
+plt.ylabel("Average")
