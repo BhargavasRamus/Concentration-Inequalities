605b0a1f8f2c581ec2d0e7aae0b4e3fe73e2e31c
Pamula Bhargav Ram <29677592+BhargavasRamus@users.noreply.github.com>
2019-01-20 19:08:07 +0530

    Update Q_5d.py

--- Assignment_1/Q_5d.py
+++ Assignment_1/Q_5d.py
@@ -1,1 +1,72 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+n = 1000
+N = 4                   #number of random varibles
+u = 0                   #mean
+v = 1  					#variance
+p = 0.5					#p in binomial               
+
+X = np.linspace(0,1,218)
+x1 = np.zeros((n))
+x2 = np.zeros((n))
+x3 = np.zeros((n))
+x4 = np.zeros((n))
+
+#guassian distribution
+for i in range(n):
+  for j in range(N):
+		x1[i] += np.random.randn()
+	x1[i] *= 1/N
+
+#uniform distribution
+for i in range(n):
+	for j in range(N):
+		x2[i] += np.random.random()
+	x2[i] *= 1/N
+
+#exponential distribution
+for i in range(n):
+	for j in range(N):
+		x3[i] += np.random.exponential(u)
+	x3[i] *= 1/N
+
+#binomial distribution
+for i in range(n):
+	for j in range(N):
+		x4[i] += np.random.binomial(N,p,n)
+	x4[i] *= 1/N
+
+#CLT
+var = v/N
+guassian = (1/np.sqrt(2*np.pi*var))*np.exp(-(X-u)**2/(2*var))
+
+plt.subplpot(4, 1, 1)
+plt.plot(X,guassian,color='#A60628',label="CLT",x1,color='#348ABD',label="%d RVS"%(N))
+plt.xlabel('x')
+plt.xlimm([0,1])
+leg = plt.legend(loc="upper right")
+leg.get_frame().set_alpha(0.1)
+
+plt.subplpot(4, 1, 2)
+plt.plot(X,guassian,color='#A60628',label="CLT",x2,color='#348ABD',label="%d RVS"%(N))
+plt.xlabel('x')
+plt.xlimm([0,1])
+leg = plt.legend(loc="upper right")
+leg.get_frame().set_alpha(0.1)
+
+plt.subplpot(4, 1, 3)
+plt.plot(X,guassian,color='#A60628',label="CLT",x3,color='#348ABD',label="%d RVS"%(N))
+plt.xlabel('x')
+plt.xlimm([0,1])
+leg = plt.legend(loc="upper right")
+leg.get_frame().set_alpha(0.1)
+
+plt.subplpot(4, 1, 4)
+plt.plot(X,guassian,color='#A60628',label="CLT",x4,color='#348ABD',label="%d RVS"%(N))
+plt.xlabel('x')
+plt.xlimm([0,1])
+leg = plt.legend(loc="upper right")
+leg.get_frame().set_alpha(0.1)
+
+plt.show()
