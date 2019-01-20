6a8b0d2f00bcc500947f5298773ecad727585d36
Pamula Bhargav Ram <29677592+BhargavasRamus@users.noreply.github.com>
2019-01-20 23:05:47 +0530

    Update Q_5d.py

--- Assignment_1/Q_5d.py
+++ Assignment_1/Q_5d.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 1000
-N = 4                   #number of random varibles
-u = 0                   #mean
-v = 1  					#variance
-p = 0.5					#p in binomial               
+N = 4
+u = 0
+v = 1
+p = 0.5
 
 X = np.linspace(0,1,218)
 x1 = np.zeros((n))
@@ -42,28 +42,32 @@ var = v/N
 guassian = (1/np.sqrt(2*np.pi*var))*np.exp(-(X-u)**2/(2*var))
 
 plt.subplpot(4, 1, 1)
-plt.plot(X,guassian,color='#A60628',label="CLT",x1,color='#348ABD',label="%d RVS"%(N))
+plt.plot(X,guassian,color='#A60628',label="CLT")
+plt.hist(x1,100,color='#348ABD',label="%d RVS"%(N))
 plt.xlabel('x')
 plt.xlimm([0,1])
 leg = plt.legend(loc="upper right")
 leg.get_frame().set_alpha(0.1)
 
 plt.subplpot(4, 1, 2)
-plt.plot(X,guassian,color='#A60628',label="CLT",x2,color='#348ABD',label="%d RVS"%(N))
+plt.plot(X,guassian,color='#A60628',label="CLT")
+plt.hist(x2,100,color='#348ABD',label="%d RVS"%(N))
 plt.xlabel('x')
 plt.xlimm([0,1])
 leg = plt.legend(loc="upper right")
 leg.get_frame().set_alpha(0.1)
 
 plt.subplpot(4, 1, 3)
-plt.plot(X,guassian,color='#A60628',label="CLT",x3,color='#348ABD',label="%d RVS"%(N))
+plt.plot(X,guassian,color='#A60628',label="CLT")
+plt.hist(x3,100,color='#348ABD',label="%d RVS"%(N))
 plt.xlabel('x')
 plt.xlimm([0,1])
 leg = plt.legend(loc="upper right")
 leg.get_frame().set_alpha(0.1)
 
 plt.subplpot(4, 1, 4)
-plt.plot(X,guassian,color='#A60628',label="CLT",x4,color='#348ABD',label="%d RVS"%(N))
+plt.plot(X,guassian,color='#A60628',label="CLT")
+plt.hist(x4,100,color='#348ABD',label="%d RVS"%(N))
 plt.xlabel('x')
 plt.xlimm([0,1])
 leg = plt.legend(loc="upper right")
